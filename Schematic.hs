3fb4f176c1c6d773f794ff2edb5cc6cdcc5342e9
Oregu <oleg.burykin@gmail.com>
2010-08-29 18:08:34 +0400

    Add parsing of simple values, bools and lists

--- Schematic.hs
+++ Schematic.hs
@@ -1,8 +1,15 @@
 module Schematic where
 
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Parse
 
-main :: IO ()
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+                   Left err -> "No match: " ++ show err
+                   Right val -> "Found value!"
+
+main :: IO()
 main = do
   args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0 ++ "!")
+  putStrLn (readExpr (args !! 0))
